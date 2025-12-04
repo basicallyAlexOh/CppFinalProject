d958a800b4f8d02b07c17f5e482b423cac96007e
anncli <55011372+anncli@users.noreply.github.com>
2025-12-03 22:04:03 -0500

    Add SoX Comparison

--- test/README.md
+++ test/README.md
@@ -4,30 +4,35 @@ Each measurement is ran three times and averaged. `-O3` flag is used for optimiz
 
 Note: The folder containing the .wav files used for measurement testing is too big for GitHub. The zipped version can be downloaded [here](https://drive.google.com/file/d/1EDftVz8lmKevOPQ8x0MFaowk3A-2bbnf/view?usp=sharing) (LionMail required, request access if needed). Place this folder under audio_samples.
 
-## `TrackManager::combineAll` Measurements
-
-### Time to combine three .wav files of increasing duration
-| Audio Duration (seconds) | Time (ms) |
-| ------------------------ | --------  |
-| **10**                   | 4.79158   |
-| **30**                   | 10.9303   |
-| **60**                   | 22.6066   |
-| **120**                  | 47.8018   |
-| **300**                  | 133.816   |
-| **600**                  | 774.993   |
+## Time to combine three .wav files of increasing duration
+| Audio Duration (seconds) | `TrackManager::combineAll`  (ms) | SoX Library (ms) |
+| ------------------------ | -------------------------------- | ---------------- |
+| **10**                   | 26.4191                          | 30.0046          |
+| **30**                   | 58.8263                          | 79.2119          |
+| **60**                   | 146.572                          | 130.122          |
+| **120**                  | 253.381                          | 248.398          |
+| **300**                  | 1360.65                          | 664.975          |
+| **600**                  | 3018.96                          | 1308.91          |
 
 <img src="measurement_results/durationResults.png" alt="durationResults.png" width="500"/>
 
 
-### Time to combine increasing number of .wav files
+## Time to combine increasing number of .wav files
 | Num WAV Files | Time (ms) |
 | ------------  | --------  |
-| **1**         | 18.1297   |
 | **2**         | 19.4643   |
 | **5**         | 24.3264   |
 | **10**        | 32.1325   |
 | **20**        | 59.8828   |
 | **50**        | 403.936   |
+
+| Num WAV Files | `TrackManager::combineAll`  (ms) | SoX Library (ms) |
+| ------------- | -------------------------------- | ---------------- |
+| **2**         | 89.0743                          | 106.205          |
+| **5**         | 164.279                          | 169.964          |
+| **10**        | 346.398                          | 328.134          |
+| **20**        | 771.555                          | 739.919          |
+| **50**        | 2354.65                          | 2191.38          |
 
 <img src="measurement_results/numFilesResults.png" alt="numFilesResults.png" width="500"/>
 
--- test/TimeCombineWav.cpp
+++ test/TimeCombineWav.cpp
@@ -3,39 +3,60 @@
 #include <iostream>
 #include <string>
 
+static const std::string pathPrefix = "audio_samples/timed_samples/";
+
 auto test_durations(std::string duration) {
+    auto start = std::chrono::steady_clock::now();
     TrackManager tm(48000);
 
-    std::string path_prefix = "audio_samples/timed_samples/";
+    tm.addTrack(pathPrefix + "drums_" + duration + ".wav");
+    tm.addTrack(pathPrefix + "acoustics_" + duration + ".wav");
+    tm.addTrack(pathPrefix + "shaker_" + duration + ".wav");
 
-    tm.addTrack(path_prefix + "drums_" + duration + ".wav");
-    tm.addTrack(path_prefix + "acoustics_" + duration + ".wav");
-    tm.addTrack(path_prefix + "shaker_" + duration + ".wav");
-
-    auto start = std::chrono::steady_clock::now();
     auto combined = tm.combineAll();
+    combined->saveToWav("combined.wav");
     auto end = std::chrono::steady_clock::now();
 
-    combined->saveToWav("combined.wav");
+    std::chrono::duration<double> elapsed = end - start;
+    return elapsed.count();
+}
+
+auto test_durations_sox(std::string duration) {
+    auto start = std::chrono::steady_clock::now();
+    system(("sox -m " + pathPrefix + "drums_" + duration + ".wav " + 
+            pathPrefix + "acoustics_" + duration + ".wav " + 
+            pathPrefix + "shaker_" + duration + ".wav combined.wav").c_str());
+    auto end = std::chrono::steady_clock::now();
 
     std::chrono::duration<double> elapsed = end - start;
     return elapsed.count();
 }
 
 auto test_num_wavs(int num_wavs) {
+    auto start = std::chrono::steady_clock::now();
     TrackManager tm(48000);
 
-    std::string path_prefix = "audio_samples/timed_samples/";
-
     for (int i = 0; i < num_wavs; ++i) {
-        tm.addTrack(path_prefix + "acoustics_60s.wav");
+        tm.addTrack(pathPrefix + "acoustics_60s.wav");
+    }
+
+    auto combined = tm.combineAll();
+    combined->saveToWav("combined.wav");
+    auto end = std::chrono::steady_clock::now();
+
+    std::chrono::duration<double> elapsed = end - start;
+    return elapsed.count();
+}
+
+auto test_num_wavs_sox(int num_wavs) {
+    std::string tracks = "";
+    for (int i = 0; i < num_wavs; ++i) {
+        tracks += pathPrefix + "acoustics_60s.wav ";
     }
 
     auto start = std::chrono::steady_clock::now();
-    auto combined = tm.combineAll();
+    system(("sox -m " + tracks + "combined.wav").c_str());
     auto end = std::chrono::steady_clock::now();
-
-    combined->saveToWav("combined.wav");
 
     std::chrono::duration<double> elapsed = end - start;
     return elapsed.count();
@@ -44,26 +65,39 @@ auto test_num_wavs(int num_wavs) {
 int main() {
     constexpr int ITERATIONS = 3;
 
+    std::cout << "=== TIME TO COMBINE .WAV FILES OF INCREASING DURATION ===\n";
     constexpr std::array<std::string, 6> TEST_DURATIONS = {
         "10s", "30s", "60s", "120s", "300s", "600s"
     };
     for (const std::string& duration : TEST_DURATIONS) {
-        double duration_sum = 0.0;
+        double trackManagerSum = 0.0;
+        double soxSum = 0.0;
+
+        std::cout << "File Length: " << duration << "\n";
 
         for (int i = 0; i < ITERATIONS; ++i) {
-            duration_sum += test_durations(duration);
+            trackManagerSum += test_durations(duration);
+            soxSum += test_durations_sox(duration);
         }
-        std::cout << "Combined " << duration << " in " << duration_sum / ITERATIONS << " seconds on average.\n";
+        std::cout << "- Avg " << trackManagerSum / ITERATIONS << " seconds to combine with TrackManager.\n";
+        std::cout << "- Avg " << soxSum / ITERATIONS << " seconds to combine with SoX.\n";
     }
+    std::cout << "\n";
 
-    constexpr std::array<int, 6> TEST_NUM_WAVS = {1, 2, 5, 10, 20, 50};
+    std::cout << "=== TIME TO COMBINE INCREASING NUM OF .WAV FILES ===\n";
+    constexpr std::array<int, 5> TEST_NUM_WAVS = {2, 5, 10, 20, 50};
     for (const int num_wavs : TEST_NUM_WAVS) {
-        double num_wavs_sum = 0.0;
+        double trackManagerSum = 0.0;
+        double soxSum = 0.0;
+
+        std::cout << "Num Files: " << num_wavs << "\n";
 
         for (int i = 0; i < ITERATIONS; ++i) {
-            num_wavs_sum += test_num_wavs(num_wavs);
+            trackManagerSum += test_num_wavs(num_wavs);
+            soxSum += test_num_wavs_sox(num_wavs);
         }
-        std::cout << "Combined " << num_wavs << " WAVs in " << num_wavs_sum / ITERATIONS << " seconds on average.\n";
+        std::cout << "- Avg " << trackManagerSum / ITERATIONS << " seconds to combine with TrackManager.\n";
+        std::cout << "- Avg " << soxSum / ITERATIONS << " seconds to combine with SoX.\n";
     }
 
     return 0;
